da718897862cf7f502e42fc60b4e8a56453c603a
Ishita Gupta <guptaishita195@gmail.com>
2026-09-26 14:35:34 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 878842-the-if-statement-GFG/878842-the-if-statement.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // code here
+    int n;
+    cin>>n;
+    if(n>100){
+        cout<<"Big"<<"\n";
+        cout<<"Number";
+    }
+    else{
+        cout<<"Number";
+    }
+
+    return 0;
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
