ca541a647f17f4116d9c1892d1fb19a95217cca6
Ishita Gupta <guptaishita195@gmail.com>
2026-09-26 14:42:10 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 700796-learn-to-comment-GFG/700796-learn-to-comment.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+    int c;
+    cin >> a >> b >> c;
+
+    // Prints a
+    cout << a << endl;
+
+    // Prints b
+    // cout << b << endl;
+
+    // Prints c
+    cout << c << endl;
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
